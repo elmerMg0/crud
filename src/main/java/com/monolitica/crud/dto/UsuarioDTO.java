39b6c66c3f0ac6d5ff34da7e30563c46815b9bc1
elmerMg0 <201801644@est.umss.edu>
2023-11-21 12:01:15 -0400

    user and rol DTO created

---
+++ src/main/java/com/monolitica/crud/dto/UsuarioDTO.java
@@ -0,0 +1,100 @@
+package com.monolitica.crud.dto;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class UsuarioDTO implements Serializable {
+    private static final long serialVersionUID = 3234234545454L;
+
+    private Integer id;
+    private String username;
+    private String password;
+
+    private String email;
+    
+
+    private String firstName;
+    private String lastName;
+
+    private Integer age;
+
+    private Date birthdate;
+
+
+
+    private String created_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+}
+
